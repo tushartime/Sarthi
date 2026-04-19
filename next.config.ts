5b2cd9e13a07d677f7cc47e808cacc954854ca0c
tushartime <timetusharindia@gmail.com>
2026-04-20 01:23:26 +0530

    security: Next.js 15.2.8, lazy-load VAD, quiet onnx warnings
    - Bump next and eslint-config-next to 15.2.8 (patched RSC advisories)
    - Load @ricky0123/vad-web via dynamic import in SalesBreakout
    - Webpack ignoreWarnings for onnxruntime-web if needed

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  webpack: (config) => {
+    config.ignoreWarnings = [
+      ...(config.ignoreWarnings ?? []),
+      { module: /node_modules[\\/]onnxruntime-web[\\/]/ },
+    ];
+    return config;
+  },
 };
 
 export default nextConfig;
